a72ada2b64bc7378b4635a8d5eaec30168ac081a
Jorge Monge <jlo4321jl@gmail.com>
2021-06-27 15:32:16 -0600

    Add header component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
-
+import Header from './components/Header'
 function App() {
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header/>
     </div>
   );
 }
