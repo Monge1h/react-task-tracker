c1368057c1e207112029a6ca28d718ae241cbb80
Jorge Monge <jlo4321jl@gmail.com>
2021-06-27 19:07:32 -0600

    Add button component to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import Button from './Button'
 const Header = ({title}) => {
+	const onClick = () => {
+		alert('Hola prros')
+	}
 	return (
-		<header>
+		<header className='header'>
 			<h1>{title}</h1>	
+			<Button onClick={onClick}/>
 		</header>
 	)
 }
