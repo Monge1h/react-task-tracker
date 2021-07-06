a97c72f65d41e57cabe768c32c505d2e0618e064
Jorge Monge <jlo4321jl@gmail.com>
2021-07-06 16:02:29 -0600

    Change name of fetchTask to fetchTasks

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
 
 	useEffect(()=>{
 		const getTask = async () => {
-			const taskFromServer = await fetchTask()
+			const taskFromServer = await fetchTasks()
 			setTasks(taskFromServer)
 		}
 		getTask()
 	},[])
 
-	const fetchTask = async () => {
+	const fetchTasks = async () => {
 		const res = await fetch('http://localhost:5000/tasks')
 		const data = await res.json()
 		return data
